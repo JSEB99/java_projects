684e5a3cd5f7ecdd91319a637df876cbce603c64
Sebastian Mora <sebastian.mt99@gmail.com>
2023-07-11 17:38:31 -0500

    while practice

---
+++ while_pr.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class while_pr {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+
+        while(name.isBlank()){
+            System.out.print("Enter your name: ");
+            name = sc.nextLine();
+        }
+
+        System.out.println("Hello "+name);
+
+    }
+}
